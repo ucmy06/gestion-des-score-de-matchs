678041c7e0468dffe5a59708915d7fd6f6bd8739
ucmy06 <julienguenoukpati825@gmail.com>
2024-09-05 06:17:01 +0000

    ajout de demande de suppression

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,7 @@
         @if(auth()->check() && auth()->user()->role === 'admin')
             <p class="text-center text-lg text-gray-800 mb-4">Vous êtes connecté en tant qu'administrateur.</p>
             <div class="flex justify-center space-x-4">
-                <a href="{{ route('admin.dashboard') }}" class="bg-indigo-600 text-white py-2 px-4 rounded-md shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-150 ease-in-out">Tableau de Bord Admin</a>
-                <a href="{{ route('admin.employees.index') }}" class="bg-gray-600 text-white py-2 px-4 rounded-md shadow hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 transition duration-150 ease-in-out">Gérer les Employés</a>
-                <a href="{{ route('admin.matches.index') }}" class="bg-blue-600 text-white py-2 px-4 rounded-md shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 ease-in-out">Gérer les Matchs</a>
+
             </div>
         @elseif(auth()->check() && auth()->user()->role === 'employee')
             <p class="text-center text-lg text-gray-800 mb-4">Vous êtes connecté en tant qu'employé.</p>
